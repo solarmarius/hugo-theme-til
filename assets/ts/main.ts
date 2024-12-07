faac405ced1aa8840a927941f5f8e2e56e5438fb
solarmarius <mso270@post.uit.no>
2024-12-07 13:20:36 +0100

    First commit

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,5 +1,3 @@
-import ContentNetworkGraph from './components/ContentNetworkGraph';
 import ClipboardCopier from './components/ClipboardCopier';
 
-customElements.define('content-network-graph', ContentNetworkGraph);
 customElements.define('clipboard-copy', ClipboardCopier);
